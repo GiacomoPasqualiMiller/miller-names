88fd85a03b5fbbf0afa2a054c9215594ae862a9e
GiacomoPasqualiMiller <giacomo.pasquali@millergroup.it>
2022-08-14 08:32:35 +0200

    ottimizzazione controllo correttezza

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,37 +2,68 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const miller = [
+  /*  {
+    Id: 100,
+    Nome: "giacomo",
+    Cognome: "sss",
+    Sezione: "tech",
+  }, */
   {
-    Nome: "jack",
+    Id: 1,
+    Nome: "giacomo",
     Cognome: "pasquali",
     Sezione: "tech",
   },
   {
-    Nome: "fede",
+    Id: 2,
+    Nome: "federico",
     Cognome: "barenghi",
     Sezione: "tech",
   },
   {
-    Nome: "andrea",
+    Id: 3,
+    Nome: "andrea ilaria",
     Cognome: "leoni",
     Sezione: "tech",
   },
   {
+    Id: 4,
+    Nome: "william",
+    Cognome: "piccione",
+    Sezione: "tech",
+  },
+  {
+    Id: 5,
+    Nome: "martina",
+    Cognome: "rosti",
+    Sezione: "tech",
+  },
+  {
+    Id: 6,
+    Nome: "mattia",
+    Cognome: "pavesi",
+    Sezione: "tech",
+  },
+  {
+    Id: 7,
+    Nome: "ian",
+    Cognome: "cavenaghi",
+    Sezione: "tech",
+  },
+  {
+    Id: 8,
     Nome: "paola",
     Cognome: "gatti",
     Sezione: "accounting",
   },
   {
+    Id: 9,
     Nome: "sara",
     Cognome: "manunza",
     Sezione: "paghe",
   },
   {
-    Nome: "sara",
-    Cognome: "manunza",
-    Sezione: "paghe",
-  },
-  {
+    Id: 10,
     Nome: "carlotta",
     Cognome: "tronconi",
     Sezione: "sicurezza",
@@ -42,6 +73,8 @@ const miller = [
 const Home = () => {
   const [goodSticker, setGoodSticker] = useState(false);
   const [badSticker, setBadSticker] = useState(false);
+  const [happySticker, setHappySticker] = useState(false);
+  const [kissSticker, setKissSticker] = useState(false);
   const [millerObj, setMillerObj] = useState([]);
   const [millerNome, setMillerNome] = useState([]);
   const [millerCognome, setMillerCognome] = useState([]);
@@ -53,47 +86,41 @@ const Home = () => {
   const handleCheck = () => {
     verifyName(millerNome, millerCognome);
   };
-
+  const goodStickerPop = () => {
+    setGoodSticker(true);
+    const timer = setTimeout(() => {
+      setGoodSticker(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  };
+  const badStickerPop = () => {
+    setBadSticker(true);
+    const timer = setTimeout(() => {
+      setBadSticker(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  };
   const verifyName = (nomePersona, cognomePersona) => {
-    if (nomePersona == "") {
-      if (cognomePersona == "") {
-        alert("Scrivi un Nome esistente!");
-      }
+    //Controlllo
+    if (nomePersona == "" && cognomePersona == "") {
+      badStickerPop();
     } else {
-      //cambio nome search in searchNome
-      if (nomePersona[0].Nome === searchNome) {
-        setGoodSticker(true);
-        console.log("Nome corretto!");
-        const timer = setTimeout(() => {
-          setGoodSticker(false);
-        }, 1000);
-        return () => clearTimeout(timer);
+      if (
+        nomePersona[0].Nome === searchNome &&
+        cognomePersona[0].Cognome === searchCognome &&
+        nomePersona[0].Id === cognomePersona[0].Id
+      ) {
+        if (nomePersona[0].Id === 3) {
+          setKissSticker(true);
+          const timer = setTimeout(() => {
+            setKissSticker(false);
+          }, 1000);
+          return () => clearTimeout(timer);
+        } else {
+          goodStickerPop();
+        }
       } else {
-        setBadSticker(true);
-        const timer = setTimeout(() => {
-          setBadSticker(false);
-        }, 1000);
-        return () => clearTimeout(timer);
-      }
-    }
-    if (cognomePersona == "") {
-      if (nomePersona == "") {
-        alert("Scrivi un Cognome esistente!");
-      }
-    } else {
-      if (cognomePersona[0].Cognome === searchCognome) {
-        setGoodSticker(true);
-        console.log("Cognome corretto!");
-        const timer = setTimeout(() => {
-          setGoodSticker(false);
-        }, 1000);
-        return () => clearTimeout(timer);
-      } else {
-        setBadSticker(true);
-        const timer = setTimeout(() => {
-          setBadSticker(false);
-        }, 1000);
-        return () => clearTimeout(timer);
+        badStickerPop();
       }
     }
   };
@@ -121,6 +148,11 @@ const Home = () => {
       } else {
         setErrorNome(true);
       }
+
+      /* Controllo Nome Uguale */
+      if (millerX.length > 1) {
+        console.log("doppio nome");
+      }
     }
   };
   const handleChangeCognome = (e) => {
@@ -138,7 +170,6 @@ const Home = () => {
           millerX.push(miller[i]);
         }
       }
-      console.log(millerX);
       setMillerCognome(millerX);
 
       /* Cambio colore Input Cognome*/
@@ -160,10 +191,9 @@ const Home = () => {
     <>
       <div className="h-screen flex-col flex flex-wrap justify-center bg-black">
         <span className="block text-xl text-center font-medium text-slate-200 mb-10">
-            Web App in costruzione...
-            <br /> Inserire solo nome o cognome e poi controllare!
+          Web App in costruzione...
         </span>
-        <form className="flex align-middle w-full justify-center">
+        <form className="flex flex-wrap align-middle w-full justify-center">
           <label className="text-center m-1 space-y-2">
             <span className="block text-2xl font-medium text-slate-500">
               Nome
@@ -172,20 +202,22 @@ const Home = () => {
               style={{ borderColor: errorNome ? "green" : "red" }}
               className="rounded-md border-4 p-1 px-2 text-center focus:outline-none"
               onChange={handleChangeNome}
-              />
+            />
           </label>
           <label className="text-center m-1 space-y-2">
             <span className="block text-2xl font-medium text-slate-500">
               Cognome
             </span>
-            <input 
-            style={{ borderColor: errorCognome ? "green" : "red" }}
-            className="rounded-md border-4 p-1 px-2 text-center focus:outline-none" onChange={handleChangeCognome} />
+            <input
+              style={{ borderColor: errorCognome ? "green" : "red" }}
+              className="rounded-md border-4 p-1 px-2 text-center focus:outline-none"
+              onChange={handleChangeCognome}
+            />
           </label>
         </form>
         <div className="flex mx-auto p-4">
           <button
-            style={{ backgroundColor: errorNome ? "green" : "red"}}
+            style={{ backgroundColor: errorNome ? "green" : "red" }}
             onClick={() => handleCheck()}
             className="bg-green-600 rounded-md p-2 text-white font-bold uppercase shadow-md"
           >
@@ -195,7 +227,12 @@ const Home = () => {
       </div>
       {/* Image container */}
       <div
-        style={{ display: goodSticker || badSticker ? "flex" : "none" }}
+        style={{
+          display:
+            goodSticker || badSticker || happySticker || kissSticker
+              ? "flex"
+              : "none",
+        }}
         id="modal"
         className="hidden fixed top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center"
       >
@@ -208,6 +245,16 @@ const Home = () => {
         <img
           style={{ display: badSticker ? "flex" : "none" }}
           src={require("../images/bad.webp")}
+          alt=""
+        />
+        <img
+          style={{ display: happySticker ? "flex" : "none" }}
+          src={require("../images/happy.webp")}
+          alt=""
+        />
+        <img
+          style={{ display: kissSticker ? "flex" : "none" }}
+          src={require("../images/kiss.webp")}
           alt=""
         />
       </div>
