7101480b2d1eb2a14b6281cc1a814390175ba1ab
GiacomoPasqualiMiller <giacomo.pasquali@millergroup.it>
2022-08-22 12:09:56 +0200

    Aggiunta contatore e semplidicazione logica

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,28 +3,32 @@ import { useNavigate } from "react-router-dom";
 
 const miller = [
   {
-    Id: 1,
+    Id: 100,
     Nome: "giacomo",
     Cognome: "pasquali",
     Sezione: "tech",
+    Foto: "jack.webp",
+  },
+  {
+    Id: 1,
+    Nome: "alessandro",
+    Cognome: "miesi",
+    Sezione: "tech",
+    Foto: "bradipo.png",
   },
   {
     Id: 2,
     Nome: "federico",
     Cognome: "barenghi",
     Sezione: "tech",
-  },
-  {
-    Id: 3,
-    Nome: "andrea ilaria",
-    Cognome: "leoni",
-    Sezione: "tech",
+    Foto: "fede.webp",
   },
   {
     Id: 4,
     Nome: "william",
     Cognome: "piccione",
     Sezione: "tech",
+    Foto: "piccione.png",
   },
   {
     Id: 5,
@@ -124,17 +128,287 @@ const miller = [
   },
   {
     Id: 21,
+    Nome: "graziella",
+    Cognome: "negroni",
+    Sezione: "paghe",
+  },
+  {
+    Id: 22,
+    Nome: "claudia",
+    Cognome: "de paoli",
+    Sezione: "paghe",
+  },
+  {
+    Id: 23,
+    Nome: "chiara",
+    Cognome: "lapegna",
+    Sezione: "paghe",
+  },
+  {
+    Id: 24,
+    Nome: "claudia",
+    Cognome: "ganini",
+    Sezione: "paghe",
+  },
+  {
+    Id: 25,
+    Nome: "michela",
+    Cognome: "coltelli",
+    Sezione: "paghe",
+  },
+  {
+    Id: 26,
+    Nome: "barbara",
+    Cognome: "baroni",
+    Sezione: "paghe",
+  },
+  {
+    Id: 27,
+    Nome: "chiara",
+    Cognome: "gismondi",
+    Sezione: "paghe",
+  },
+  {
+    Id: 28,
+    Nome: "cinzia",
+    Cognome: "pancallo",
+    Sezione: "paghe",
+  },
+  {
+    Id: 29,
+    Nome: "grazia",
+    Cognome: "giammusso",
+    Sezione: "paghe",
+  },
+  {
+    Id: 30,
+    Nome: "valentina",
+    Cognome: "bellinzoni",
+    Sezione: "paghe",
+  },
+  {
+    Id: 31,
+    Nome: "giovanna",
+    Cognome: "lamacchia",
+    Sezione: "paghe",
+  },
+  {
+    Id: 32,
+    Nome: "nicole",
+    Cognome: "alessandri",
+    Sezione: "paghe",
+  },
+  {
+    Id: 33,
+    Nome: "rossella",
+    Cognome: "semenza",
+    Sezione: "paghe",
+  },
+  {
+    Id: 34,
+    Nome: "alessandra",
+    Cognome: "muzzin",
+    Sezione: "paghe",
+  },
+  {
+    Id: 35,
+    Nome: "simona",
+    Cognome: "nassi",
+    Sezione: "paghe",
+  },
+  {
+    Id: 36,
+    Nome: "laura",
+    Cognome: "balestrieri",
+    Sezione: "paghe",
+  },
+  {
+    Id: 37,
+    Nome: "angela",
+    Cognome: "tolentino",
+    Sezione: "paghe",
+  },
+  {
+    Id: 38,
+    Nome: "silvia",
+    Cognome: "cantarelli",
+    Sezione: "paghe",
+  },
+  {
+    Id: 39,
+    Nome: "alessandra",
+    Cognome: "zecca",
+    Sezione: "paghe",
+  },
+  {
+    Id: 40,
+    Nome: "cristina",
+    Cognome: "ladie",
+    Sezione: "paghe",
+  },
+  {
+    Id: 41,
+    Nome: "daniela",
+    Cognome: "zoppi",
+    Sezione: "paghe",
+  },
+  {
+    Id: 42,
+    Nome: "jessica",
+    Cognome: "bianchini",
+    Sezione: "paghe",
+  },
+  {
+    Id: 43,
+    Nome: "giulia",
+    Cognome: "salvatore",
+    Sezione: "paghe",
+  },
+  {
+    Id: 44,
+    Nome: "alison",
+    Cognome: "maraschi",
+    Sezione: "paghe",
+  },
+  {
+    Id: 45,
+    Nome: "alessio",
+    Cognome: "ferrario",
+    Sezione: "paghe",
+  },
+  {
+    Id: 46,
+    Nome: "dario",
+    Cognome: "mazzola",
+    Sezione: "paghe",
+  },
+  {
+    Id: 47,
+    Nome: "roberta",
+    Cognome: "mazzola",
+    Sezione: "paghe",
+  },
+  {
+    Id: 48,
+    Nome: "diego",
+    Cognome: "albergoni",
+    Sezione: "paghe",
+  },
+  {
+    Id: 49,
+    Nome: "alessandra",
+    Cognome: "sperti",
+    Sezione: "accounting",
+  },
+  {
+    Id: 50,
+    Nome: "lorenzo",
+    Cognome: "alfieri",
+    Sezione: "accounting",
+  },
+  {
+    Id: 51,
+    Nome: "elena",
+    Cognome: "ligorio",
+    Sezione: "accounting",
+  },
+  {
+    Id: 52,
+    Nome: "chiara",
+    Cognome: "monachino",
+    Sezione: "accounting",
+  },
+  {
+    Id: 53,
+    Nome: "katia",
+    Cognome: "capasso",
+    Sezione: "accounting",
+  },
+  {
+    Id: 54,
     Nome: "carlotta",
     Cognome: "tronconi",
     Sezione: "sicurezza",
+  },
+  {
+    Id: 55,
+    Nome: "andrea",
+    Cognome: "gandaglia",
+    Sezione: "sicurezza",
+    Foto: "ganda.png",
+  },
+  {
+    Id: 56,
+    Nome: "laura",
+    Cognome: "archeri",
+    Sezione: "sicurezza",
+  },
+  {
+    Id: 57,
+    Nome: "sonali",
+    Cognome: "fernando",
+    Sezione: "sicurezza",
+  },
+  {
+    Id: 58,
+    Nome: "stefania",
+    Cognome: "arena",
+    Sezione: "sicurezza",
+  },
+  {
+    Id: 59,
+    Nome: "katherin",
+    Cognome: "machuca",
+    Sezione: "sicurezza",
+  },
+  {
+    Id: 60,
+    Nome: "luca",
+    Cognome: "caffi",
+    Sezione: "sicurezza",
+  },
+  {
+    Id: 61,
+    Nome: "matteo",
+    Cognome: "caraffini",
+    Sezione: "sicurezza",
+  },
+  {
+    Id: 62,
+    Nome: "andrea",
+    Cognome: "tarchini",
+    Sezione: "sicurezza",
+  },
+  {
+    Id: 63,
+    Nome: "francesca",
+    Cognome: "corradi",
+    Sezione: "sicurezza",
+  },
+  {
+    Id: 64,
+    Nome: "chiara",
+    Cognome: "teresi",
+    Sezione: "sicurezza",
+  },
+  {
+    Id: 65,
+    Nome: "raffaele",
+    Cognome: "de vecchi",
+    Sezione: "sicurezza",
+  },
+  {
+    Id: 3000,
+    Nome: "andrea ilaria",
+    Cognome: "leoni",
+    Sezione: "tech",
+    Foto: "kiss.webp",
   },
 ];
 
 const Home = () => {
   const [goodSticker, setGoodSticker] = useState(false);
   const [badSticker, setBadSticker] = useState(false);
-  const [happySticker, setHappySticker] = useState(false);
-  const [kissSticker, setKissSticker] = useState(false);
   const [millerObj, setMillerObj] = useState([]);
   const [millerNome, setMillerNome] = useState([]);
   const [millerCognome, setMillerCognome] = useState([]);
@@ -142,23 +416,35 @@ const Home = () => {
   const [searchCognome, setSearchCognome] = useState([]);
   const [errorNome, setErrorNome] = useState();
   const [errorCognome, setErrorCognome] = useState(false);
+  const [imgPop, setImgPop] = useState();
+  const [timerPop, setTimerPop] = useState(false);
 
   const handleCheck = () => {
     verifyName(millerCognome);
   };
-  const goodStickerPop = () => {
-    setGoodSticker(true);
+  const goodStickerPop = (cognomeSelect) => {
+    const x = { ...cognomeSelect };
+    x[0].Foto = "good.webp";
+    setImgPop(x[0].Foto);
+    setTimerPop(true);
     const timer = setTimeout(() => {
-      setGoodSticker(false);
+      setTimerPop(false);
     }, 1000);
-    return () => clearTimeout(timer);
+    reloadPage();
   };
   const badStickerPop = () => {
-    setBadSticker(true);
+    const x = { Foto: "bad.webp" };
+    setImgPop(x.Foto);
+    setTimerPop(true);
     const timer = setTimeout(() => {
-      setBadSticker(false);
+      setTimerPop(false);
     }, 1000);
-    return () => clearTimeout(timer);
+    reloadPage();
+  };
+  const reloadPage = () => {
+    setTimeout(() => {
+      window.location.reload();
+    }, 500);
   };
   const verifyName = (cognomeSelect) => {
     //Controlllo
@@ -169,22 +455,21 @@ const Home = () => {
         cognomeSelect[0].Cognome === searchCognome &&
         cognomeSelect[0].Nome === searchNome
       ) {
-        if (cognomeSelect[0].Id === 3) {
-          setKissSticker(true);
+        if (cognomeSelect[0].Foto) {
+          setImgPop(cognomeSelect[0].Foto);
+          setTimerPop(true);
           const timer = setTimeout(() => {
-            setKissSticker(false);
+            setTimerPop(false);
           }, 1000);
-          return () => clearTimeout(timer);
         } else {
-          goodStickerPop();
+          goodStickerPop(cognomeSelect);
         }
       } else {
         badStickerPop();
+        reloadPage();
       }
     }
-    setTimeout(() => {
-      window.location.reload();
-    }, 500);
+    reloadPage();
   };
 
   const handleChangeNome = (e) => {
@@ -200,10 +485,10 @@ const Home = () => {
         ) {
           setSearchNome(e.target.value.toLowerCase());
           millerX.push(miller[i]);
+          console.log(millerX);
         }
       }
       setMillerNome(millerX);
-
       /* Cambio colore Input Nome*/
       if (millerX == "") {
         setErrorNome(false);
@@ -250,7 +535,9 @@ const Home = () => {
   return (
     <>
       <div className="flex-col flex flex-wrap justify-center bg-black p-2 pt-4">
-        <h1 className="text-white text-center text-4xl font-bold">MILLER NAMES</h1>
+        <h1 className="text-white text-center text-4xl font-bold">
+          MILLER NAMES
+        </h1>
         <NameGame rightAnswer={goodStickerPop} badAnswer={badStickerPop} />
         <SurnameGame rightAnswer={goodStickerPop} badAnswer={badStickerPop} />
         <span className=" mt-10">Indovina sia il Nome che il Cognome</span>
@@ -287,38 +574,24 @@ const Home = () => {
         </div>
       </div>
       <div></div>
-      {/* Image container */}
-      <div
-        style={{
-          display:
-            goodSticker || badSticker || happySticker || kissSticker
-              ? "flex"
-              : "none",
-        }}
-        id="modal"
-        className="fixed top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center"
-      >
-        <img
-          style={{ display: goodSticker ? "flex" : "none" }}
-          src={require("../images/good.webp")}
-          alt=""
-        />
-        <img
-          style={{ display: badSticker ? "flex" : "none" }}
-          src={require("../images/bad.webp")}
-          alt=""
-        />
-        <img
-          style={{ display: happySticker ? "flex" : "none" }}
-          src={require("../images/happy.webp")}
-          alt=""
-        />
-        <img
-          style={{ display: kissSticker ? "flex" : "none" }}
-          src={require("../images/kiss.webp")}
-          alt=""
-        />
-      </div>
+      {timerPop
+        ? (console.log(`../images/${imgPop}`),
+          (
+            <div
+              style={{
+                display: timerPop ? "flex" : "none",
+              }}
+              id="modal"
+              className="fixed top-0 left-0 z-80 w-screen h-screen bg-black/70 flex justify-center items-center"
+            >
+              <img
+                style={{ display: timerPop ? "flex" : "none" }}
+                src={require(`../images/${imgPop}`)}
+                alt=""
+              />
+            </div>
+          ))
+        : ""}
     </>
   );
 };
@@ -328,12 +601,15 @@ const NameGame = (props) => {
   const [cognomeCasuale, setCognomeCasuale] = useState([]);
   const [nomeValue, setNomeValue] = useState([]);
   const [updated, setUpdated] = useState(false);
+  const [count, setCount] = useState(0);
+
+  localStorage.setItem("nomi_indovinati", count);
   const handleChange = (e) => {
     setNomeValue(e.target.value.toLowerCase());
   };
   const verifyName = (e) => {
     if (nomeValue === miller[cognomeCasuale].Nome) {
-      props.rightAnswer();
+      setCount(count + 1);
     } else {
       props.badAnswer();
     }
@@ -354,6 +630,9 @@ const NameGame = (props) => {
   return (
     <>
       <span className=" mt-10">Indovina il Nome</span>
+      <span className="font-bold text-2xl">
+        {localStorage.getItem("nomi_indovinati")}
+      </span>
       <form className="flex flex-wrap align-middle w-full justify-center">
         <label className="text-center m-1 space-y-2">
           <span className="block text-2xl font-medium text-slate-500">
@@ -400,13 +679,17 @@ const SurnameGame = (props) => {
   const [nomeCasuale, setNomeCasuale] = useState([]);
   const [cognomeValue, setCognomeValue] = useState([]);
   const [updated, setUpdated] = useState(false);
+  const [count, setCount] = useState(0);
+
+  localStorage.setItem("cognomi_indovinati", count);
+
   const handleChange = (e) => {
     setCognomeValue(e.target.value.toLowerCase());
     console.log(miller[nomeCasuale]);
   };
   const verifyName = (e) => {
     if (cognomeValue === miller[nomeCasuale].Cognome) {
-      props.rightAnswer();
+      setCount(count + 1);
     } else {
       props.badAnswer();
     }
@@ -427,6 +710,9 @@ const SurnameGame = (props) => {
   return (
     <>
       <span className=" mt-10">Indovina il Cognome</span>
+      <span className="font-bold text-2xl">
+        {localStorage.getItem("cognomi_indovinati")}
+      </span>
       <form className="flex flex-wrap align-middle w-full justify-center">
         <label className="text-center m-1 space-y-2">
           <span className="block text-2xl font-medium text-slate-500">
